6a62457c007bd289edde5c4c778c9df54a161743
Amit Pandit <90578817+amit0693@users.noreply.github.com>
2021-10-08 09:36:07 -0400

    Update HangMan.py

--- HangMan.py
+++ HangMan.py
@@ -1,27 +1,25 @@
 import random
-import Stages_of_Ascii_hangman
 from Stages_of_Ascii_hangman import stages
-end_of_game=False
-word_list = ["ardvark", "baboon", "camel"]
-chosen_word = random.choice(word_list)
-lives=6
-print(f'Pssst, the solution is {chosen_word}.')
+end = False
+fruits= ['apple','orange','banana','mango','grape','strawberry']
+random_fruit = random.choice(fruits)
+lives = 6
 display = []
-for _ in range(len(chosen_word)):
+for _ in range(len(random_fruit)):
     display += "_"
-while not end_of_game:
+while not end:
     guess = input("Guess a letter: ").lower()
-    for position in range(len(chosen_word)):
-        letter = chosen_word[position]
+    for position in range(len(random_fruit)):
+        letter = random_fruit[position]
         if letter == guess:
             display[position] = letter
-    if guess not in chosen_word:
-        lives-=1
-        if lives==0:
-            end_of_game==True
-            print("You loose")
+    if guess not in random_fruit:
+        lives -= 1
+        if lives == 0:
+            end = True
+            print("You lose.")
     print(f"{' '.join(display)}")
     if "_" not in display:
-        end_of_game = True
+        end = True
         print("You win.")
     print(stages[lives])
